a77099238bfdac40e7333b15e975ec23bba20c55
bjarte <bjartejensen@gmail.com>
2021-01-21 20:30:06 +0100

    incl remove of envir

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiPath: "https://us-central1-vaerkstedet-ae5e0.cloudfunctions.net/api/",
-  mailPath:"https://us-central1-pencil-test-98d8d.cloudfunctions.net/api/email/send"
-
-};
